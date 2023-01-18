2fb68296704aad2e316147e3172864aa152da39c
Genario Maciel <genario.maciel@outlook.com>
2023-01-18 12:19:50 -0300

    Commit initial

---
+++ src/test/java/br/com/leicam/strategy/StrategyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.leicam.strategy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrategyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
